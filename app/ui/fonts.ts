2a3e5a37f33b30867519765beca6a17d169c938a
sporarara <159859714+sporarara@users.noreply.github.com>
2024-02-13 12:49:06 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lus = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
